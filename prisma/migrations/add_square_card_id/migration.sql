9649fe40c9d914cc50c571d309c0ae6ef91c655a
NanoProgrammer <santiagogonzalezjaimes77@gmail.com>
2026-05-16 14:51:20 -0600

    db

--- prisma/migrations/add_square_card_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable: add squareCardId to Membership for Artesano card-on-file (no subscription)
-ALTER TABLE "Membership" ADD COLUMN IF NOT EXISTS "squareCardId" TEXT;
--- prisma/migrations/add_tip_cents/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable: add tipCents to Order
-ALTER TABLE "Order" ADD COLUMN "tipCents" INTEGER NOT NULL DEFAULT 0;
